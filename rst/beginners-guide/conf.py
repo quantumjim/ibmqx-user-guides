580c14d5b7669dc905eed4cfd6de752b67d263eb
Borja Godoy Gago <borja.godoy@barrabesmeaning.com>
2018-09-17 11:12:49 +0200

    fix unicode error

--- rst/beginners-guide/conf.py
+++ rst/beginners-guide/conf.py
@@ -54,7 +54,7 @@ source_suffix = '.rst'
 master_doc = 'introduction'
 
 # General information about the project.
-project = 'Beginner’s Guide'
+project = 'Beginner\'s guide'
 copyright = '2017, IBM Research and the IBM QX team'
 author = 'Jay M. Gambetta, Lev Bishop, Antonio Córcoles, Andrew Cross'
 
